9577cbb2e50a9e1074d6160f23f0e7f8f0ca78d6
Alan Chu <achu61@seas.upenn.edu>
2024-03-21 00:49:37 -0500

    comment out some unused var

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Homepage from './Homepage';
+// import { Routes, Route } from 'react-router-dom';
+// import Homepage from './Homepage';
 function Main() {
     return (
         <main className="container-fluid">
