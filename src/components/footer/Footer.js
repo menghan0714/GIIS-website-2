8383fb51a5b0fe59637e2a76fc119c4a205690b5
Alan Chu <achu61@seas.upenn.edu>
2024-03-20 23:19:14 -0500

    init structure

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,8 @@
+function Footer() {
+    return (
+        <footer className="page-footer bg-success-subtle">
+            Copyright&copy; {new Date().getFullYear()} Alan Hwader Chu. All rights reserved.
+        </footer>
+    )
+}
+export default Footer;
---
+++ src/components/main/Main.js
@@ -0,0 +1,9 @@
+function Main() {
+    return (
+        <main className="container-fluid">
+            This is main content.
+        </main>
+    );
+}
+
+export default Main;
