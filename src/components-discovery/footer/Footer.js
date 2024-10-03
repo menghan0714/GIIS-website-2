b84f889893cdcbe3a66dd93b81baff54e98925dc
menghan0714 <144967883+menghan0714@users.noreply.github.com>
2024-10-03 16:45:48 +0800

    Update and rename test to Footer.js

---
+++ src/components-discovery/footer/Footer.js
@@ -0,0 +1,8 @@
+function Footer() {
+    return (
+        <footer className="page-footer bg-success-subtle">
+            Copyright&copy; {new Date().getFullYear()} Genesis of Ideas International School. All rights reserved.
+        </footer>
+    )
+}
+export default Footer;
