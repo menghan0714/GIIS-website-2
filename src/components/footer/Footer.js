15b3151f7222bfcdf40b03c18b573493532478c6
menghan0714 <144967883+menghan0714@users.noreply.github.com>
2024-10-05 16:44:18 +0800

    Update Footer.js

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,7 +2,7 @@ import styles from './Footer.module.css';
 
 function Footer() {
     return (
-        <footer className={styles.footer}>
+        <footer className={styles.pageFooter}>
             Copyright&copy; {new Date().getFullYear()} Genesis of Ideas International School. All rights reserved.
         </footer>
     )
