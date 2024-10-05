360d11522dc6fc005ff3a2f7c08030e79227bd3b
menghan0714 <144967883+menghan0714@users.noreply.github.com>
2024-10-05 16:43:45 +0800

    Update Footer.js

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,6 +1,8 @@
+import styles from './Footer.module.css';
+
 function Footer() {
     return (
-        <footer className="page-footer bg-success-subtle">
+        <footer className={styles.footer}>
             Copyright&copy; {new Date().getFullYear()} Genesis of Ideas International School. All rights reserved.
         </footer>
     )
