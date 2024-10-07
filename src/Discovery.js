d008d175baebb5deee02cf8551708960068506d3
menghan0714 <144967883+menghan0714@users.noreply.github.com>
2024-10-07 23:23:13 +0800

    Create Discovery.js

---
+++ src/Discovery.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Discovery() {
+    return (
+        <div>
+            <h1>Discovery Page</h1>
+            <p>Welcome to the Discovery section of the website!</p>
+        </div>
+    );
+}
+
+export default Discovery;
