9de481926154613b3c4b8df7ce19d1ac6b8c8d06
menghan0714 <144967883+menghan0714@users.noreply.github.com>
2024-10-08 00:04:30 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Header from './components/header/Header';
 import Main from './components/main/Main';
 import Footer from './components/footer/Footer';
-import Discovery from './components/pages/Discovery';
+import Discovery from './components/pages/Discovery/Discovery';
 
 function App() {
   const [language, setLanguage] = useState('en');
