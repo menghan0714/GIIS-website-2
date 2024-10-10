b9da4c62346938dc70e22fd633a5194b7730d1ce
menghan0714 <144967883+menghan0714@users.noreply.github.com>
2024-10-10 17:32:12 +0800

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,1 +1,28 @@
+import React from 'react';
+import logo_slogan from '../../img/logo_slogan.png';
+import { Link } from 'react-router-dom';
+import styles from './Header.module.css'; // Custom styles for specific adjustments
 
+function Header({ language, toggleLanguage }) {
+    return (
+        <header className={`${styles.header}`}>
+            {/* First row for logo and buttons */}
+            <div className="row align-items-center">
+                <div className="col-10 d-flex align-items-center">
+                    <Link to="/">
+                        <img src={logo_slogan} alt='Excel High School Logo' className={`img-fluid ${styles.logoImage}`} />
+                    </Link>
+                </div>
+                <div className="col-2 d-flex justify-content-center">
+                    {/* <Link to="/contact" className="btn btn-link px-2">Contact Us</Link> */}
+                    <a href="https://moodles.genesisideas.school" target="_blank" rel="noopener noreferrer" className="btn btn-link px-2">Moodles</a>
+                    <button className="btn btn-link px-2" onClick={toggleLanguage}>
+                        {language === 'en' ? 'Switch to Chinese' : '切换到英文'}
+                    </button>
+                </div>
+            </div> 
+        </header>
+    );
+}
+
+export default Header;
