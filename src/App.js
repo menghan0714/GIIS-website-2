2e5ca41793e15a9ab75496150f69e38d99b33b0e
menghan0714 <144967883+menghan0714@users.noreply.github.com>
2024-10-10 17:12:17 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+
 import Homepage from './components/pages/Homepage/Homepage';
 import Discovery from './components/pages/Discovery/Discovery';
 import Academics from './components/pages/Academics/Academics';
-import Footer from './components/Footer/Footer';
 
 function App() {
    const [language, setLanguage] = useState('en');
@@ -22,16 +24,16 @@ function App() {
   return (
     <Router>
       <Header language={language} toggleLanguage={toggleLanguage} />
-    
+      <main className="container-fluid">
+     
       <Routes>
+        <Route path="/" element={<Homepage language={language} />} />
         <Route path="/discovery" element={<Discovery language={language} />} />
-        <Route path="/" element={<Main language={language} />} />
+        <Route path="/academics" element={<Academics language={language} />} />
+        <Route path="/" element={<Academics language={language} />} />
       </Routes>
-
-      <Footer language={language} />
-   
-   <Router>
-     <h1>Hello, World!</h1>
+     </main>
+     <Footer />
    </Router>
   );
 }
