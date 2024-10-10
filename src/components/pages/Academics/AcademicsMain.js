59e85a05d47bb10bd2fbd5450ffb1d2e0876ca49
menghan0714 <144967883+menghan0714@users.noreply.github.com>
2024-10-10 18:08:19 +0800

    Update and rename Academics.js to AcademicsMain.js

---
+++ src/components/pages/Academics/AcademicsMain.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+// 一個簡單的 Academics 組件
+const Academics = () => {
+  return (
+    <section>
+      <h1>Academics</h1>
+      <p>Welcome to the Academics section! Here, we provide information about our educational programs and offerings.</p>
+
+      <div>
+        <h2>Programs Offered</h2>
+        <ul>
+          <li>Undergraduate Programs</li>
+          <li>Graduate Programs</li>
+          <li>Online Courses</li>
+          <li>Continuing Education</li>
+        </ul>
+      </div>
+
+      <div>
+        <h2>Academic Resources</h2>
+        <ul>
+          <li>Library</li>
+          <li>Research Labs</li>
+          <li>Academic Advising</li>
+          <li>Student Support Services</li>
+        </ul>
+      </div>
+    </section>
+  );
+};
+
+export default Academics;
+
