39e012bc506e4e33d394555f48e5aa4ca9265a2e
menghan0714 <144967883+menghan0714@users.noreply.github.com>
2024-10-10 18:15:32 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 
@@ -22,7 +22,6 @@ function App() {
   };
 
   return (
-    <Router>
       <Header language={language} toggleLanguage={toggleLanguage} />
       <main className="container-fluid">
      
@@ -31,9 +30,8 @@ function App() {
         <Route path="/discovery" element={<Discovery language={language} />} />
         <Route path="/academics" element={<Academics language={language} />} />
       </Routes>
-     </main>
-     <Footer />
-   </Router>
+      </main>
+      <Footer />
   );
 }
 
