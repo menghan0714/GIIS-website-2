e3ca1e45118f962091f3055ea02f82e36f16cacc
menghan0714 <144967883+menghan0714@users.noreply.github.com>
2024-10-12 01:38:43 +0800

    Update HomepageMain.js

--- src/components/pages/Homepage/HomepageMain.js
+++ src/components/pages/Homepage/HomepageMain.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Nav from './HomepageHeader/Nav.js';
 import ImgSlider from './Homepage/ImgSlider.js';
 import Slogan from './Homepage/Slogan';
 import Introduction from './Homepage/Introduction';
@@ -8,58 +9,60 @@ import FacultyGraduates from './Homepage/FacultyGraduates.js';
 import ContactForm from './Homepage/ContactForm';
 
 function Homepage({ language }) {
-    return (
-        <>
-            <div className="card mb-1" id="homepage">
-                <div className="container">
-                    <ImgSlider />
-                    <div className="card-body">
-                        <Slogan />
-                    </div>
+   return (
+    <>
+        <div className="row">
+            <Nav language={language} />
+        </div>
+
+        <div className="card mb-1" id="homepage">
+            <div className="container">
+                <ImgSlider />
+                <div className="card-body">
+                    <Slogan />
                 </div>
             </div>
+        </div>
 
-            <div className="card mb-1" id="about">
-                <div className="container">
-                    <div className="card-body">
-                        <Introduction language={language} />
-                    </div>
+        <div className="card mb-1" id="about">
+            <div className="container">
+                <div className="card-body">
+                    <Introduction language={language} />
                 </div>
             </div>
+        </div>
 
-            <div className="card mb-1" id="accreditation">
-                <div className="container">
-                    <div className="card-body">
-                        <CogniaAccreditation language={language} />
-                    </div>
+        <div className="card mb-1" id="accreditation">
+            <div className="container">
+                <div className="card-body">
+                    <CogniaAccreditation language={language} />
                 </div>
             </div>
+        </div>
 
-            <div className="card mb-1" id="testimonials">
-                <div className="container">
-                    <div className="card-body">
-                        <Testimonial />
-                    </div>
+        <div className="card mb-1" id="testimonials">
+            <div className="container">
+                <div className="card-body">
+                    <Testimonial />
                 </div>
             </div>
+        </div>
 
-            <div className="card mb-1" id="faculty">
-                <div className="container">
-                    <div className="card-body">
-                        <FacultyGraduates />
-                    </div>
+        <div className="card mb-1" id="faculty">
+            <div className="container">
+                <div className="card-body">
+                    <FacultyGraduates />
                 </div>
             </div>
+        </div>
 
-            <div className="card mb-1" id="contact">
-                <div className="container">
-                    <div className="card-body">
-                        <ContactForm />
-                    </div>
+        <div className="card mb-1" id="contact">
+            <div className="container">
+                <div className="card-body">
+                    <ContactForm />
                 </div>
             </div>
-        </>
-    );
-}
-
-export default Homepage;
+        </div>
+    </>
+);
+export default HomepageMain;
