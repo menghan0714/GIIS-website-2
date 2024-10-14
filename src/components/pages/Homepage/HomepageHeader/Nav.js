bb2eb9226f15733c7008ae201c121dcb68525a15
menghan0714 <144967883+menghan0714@users.noreply.github.com>
2024-10-14 21:47:50 +0800

    Update Nav.js

--- src/components/pages/Homepage/HomepageHeader/Nav.js
+++ src/components/pages/Homepage/HomepageHeader/Nav.js
@@ -26,10 +26,6 @@ function Nav({ language }) {
     return (
         <nav className={`navbar navbar-expand-lg ${isNavSticky ? 'fixed-top' : ''} ${styles.customBackground}`}>
             <div className={`container-fluid ${styles.navContainer}`}>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContents" aria-controls="navbarSupportedContents" aria-expanded={!isCollapsed} aria-label="Toggle navigation" onClick={toggleNavbar}>
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className={`collapse navbar-collapse ${isCollapsed ? '' : 'show'}`} id="navbarSupportedContents">
                  <ul className={`navbar-nav ${styles.customnavbar}`}>
                   <li className={styles.navitem}>
                    <Link className={styles.navLink} to="/discovery">{language === 'en' ? 'DISCOVERY' : '发现我们'}</Link>
@@ -60,7 +56,6 @@ function Nav({ language }) {
                    </ul>
                   </li>
                  </ul>
-                </div>
             </div>
          </nav>
     );
