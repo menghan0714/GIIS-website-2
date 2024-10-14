161df8ea5ffb4bb315197367d2a478b93b353f87
menghan0714 <144967883+menghan0714@users.noreply.github.com>
2024-10-14 17:58:51 +0800

    Update Nav.js

--- src/components/pages/Homepage/HomepageHeader/Nav.js
+++ src/components/pages/Homepage/HomepageHeader/Nav.js
@@ -31,7 +31,7 @@ function Nav({ language }) {
                 </button>
                 <div className={`collapse navbar-collapse ${isCollapsed ? '' : 'show'}`} id="navbarSupportedContents">
                  <ul className={`navbar-nav ${styles.customnavbar}`}>
-                  <li className=styles.navitem}><Link className={styles.navLink} to="/discovery">{language === 'en' ? 'DISCOVERY' : '发现我们'}</Link></li>
+                  <li className={styles.navitem}><Link className={styles.navLink} to="/discovery">{language === 'en' ? 'DISCOVERY' : '发现我们'}</Link></li>
                   <li className={styles.navitem}><Link className={styles.navLink} to="/academics">{language === 'en' ? 'ACADEMICS' : '学术'}</Link></li>
                   <li className={styles.navitem}><Link className={styles.navLink} to="/academics">{language === 'en' ? 'Admission' : '入学'}</Link></li>
                   <li className={styles.navitem}><Link className={styles.navLink} to="/academics">{language === 'en' ? 'Student Support' : '学生支持'}</Link></li>
