d237eeca6b6c64399107cd27048a2d42225331aa
menghan0714 <144967883+menghan0714@users.noreply.github.com>
2024-10-15 00:03:59 +0800

    Update Nav.js

--- src/components/pages/Homepage/HomepageHeader/Nav.js
+++ src/components/pages/Homepage/HomepageHeader/Nav.js
@@ -30,29 +30,32 @@ function Nav({ language }) {
                   <li className={styles.navitem}>
                    <Link className={styles.navLink} to="/discovery">{language === 'en' ? 'DISCOVERY' : '发现我们'}</Link>
                    <ul className={styles.dropdownMenu}>
-                    <li>Option 1</li>
-                    <li>Option 2</li>
+                    <li>Meet Our School</li>
+                    <li>Our Mission</li>
+                    <li>About Our Faculty</li>
                    </ul>
                   </li>
                   <li className={styles.navitem}>
                    <Link className={styles.navLink} to="/academics">{language === 'en' ? 'ACADEMICS' : '学术'}</Link>
                    <ul className={styles.dropdownMenu}>
-                    <li>Option 3</li>
-                    <li>Option 4</li>
+                    <li>Learning Style</li>
+                    <li>Subjects</li>
+                    <li>Curriculum Options</li>
                    </ul>
                   </li>
                   <li className={styles.navitem}>
                    <Link className={styles.navLink} to="/admission">{language === 'en' ? 'ADMISSION' : '入学'}</Link>
                    <ul className={styles.dropdownMenu}>
-                    <li>Option 5</li>
-                    <li>Option 6</li>
+                    <li>Apply Now</li>
+                    <li>Tuition & Fee</li>
+                    <li>Frequently Asked Question</li>
                    </ul>
                   </li>
                   <li className={styles.navitem}>
                    <Link className={styles.navLink} to="/support">{language === 'en' ? 'STUDENT SUPPORT' : '学生支持'}</Link>
                    <ul className={styles.dropdownMenu}>
-                    <li>Option 5</li>
-                    <li>Option 6</li>
+                    <li>Academic Advising</li>
+                    <li>Life Counseling</li>
                    </ul>
                   </li>
                  </ul>
