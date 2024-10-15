7c8235192d48cacb331afade521274d987be26f2
menghan0714 <144967883+menghan0714@users.noreply.github.com>
2024-10-16 00:43:37 +0800

    Update Nav.js

--- src/components/pages/Homepage/HomepageHeader/Nav.js
+++ src/components/pages/Homepage/HomepageHeader/Nav.js
@@ -30,6 +30,12 @@ function Nav({ language }) {
               <span className="navbar-toggler-icon"></span>
              </button>
              <div className={`collapse navbar-collapse ${isCollapsed ? '' : 'show'}`} id="navbarSupportedContents">
+               <ul className={styles.leftSlideMenu}>
+                <li><Link to="/discovery">{language === 'en' ? 'DISCOVERY' : '发现我们'}</Link></li>
+                <li><Link to="/academics">{language === 'en' ? 'ACADEMICS' : '学术'}</Link></li>
+                <li><Link to="/admission">{language === 'en' ? 'ADMISSION' : '入学'}</Link></li>
+                <li><Link to="/support">{language === 'en' ? 'STUDENT SUPPORT' : '学生支持'}</Link></li>
+               </ul>
                <ul className={`navbar-nav ${styles.customnavbar}`}>
                 <li className={styles.navitem}>
                   <Link className={styles.navLink} to="/discovery">{language === 'en' ? 'DISCOVERY' : '发现我们'}</Link>
