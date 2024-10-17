989caae68ed1bebbced87ccb4eff51078e9d1434
menghan0714 <144967883+menghan0714@users.noreply.github.com>
2024-10-17 13:41:07 +0800

    Update Nav.js

--- src/components/pages/Homepage/HomepageHeader/Nav.js
+++ src/components/pages/Homepage/HomepageHeader/Nav.js
@@ -32,7 +32,7 @@ function Nav({ language }) {
 
              <div className={`collapse navbar-collapse ${!isCollapsed ? 'show' : ''} ${styles.leftSlideMenu}`} id="navbarLeftMenu">
               <ul className={styles.leftSlideItems}>
-                <Link className={styles.leftSlideItemssmall} to="/discovery">
+                <Link to="/discovery">
                  <li>{language === 'en' ? 'DISCOVERY' : '发现我们'}</li>
                 </Link> 
                 <Link className to="/academics">
