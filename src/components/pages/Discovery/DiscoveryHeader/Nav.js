c114435c02e51f7de6650b66f709b4d50a2b6f9f
menghan0714 <144967883+menghan0714@users.noreply.github.com>
2024-10-18 00:59:05 +0800

    Update Nav.js

--- src/components/pages/Discovery/DiscoveryHeader/Nav.js
+++ src/components/pages/Discovery/DiscoveryHeader/Nav.js
@@ -46,16 +46,35 @@ function Nav({ language }) {
                <ul className={`navbar-nav ${styles.customnavbar}`}>
                 <li className={styles.navitem}>
                   <Link className={styles.navLink} to="/discovery">{language === 'en' ? 'DISCOVERY' : '发现我们'}</Link>
+                  <ul className={styles.dropdownMenu}>
+                    <li>Meet Our School</li>
+                    <li>Our Mission</li>
+                    <li>About Our Faculty</li>
+                  </ul>
                 </li>
                 <li className={styles.navitem}>
                   <Link className={styles.navLink} to="/academics">{language === 'en' ? 'ACADEMICS' : '学术'}</Link>
-                </li>
+                  <ul className={styles.dropdownMenu}>
+                    <li>Learning Style</li>
+                    <li>Subjects</li>
+                    <li>Curriculum Options</li>
+                  </ul>
+                 </li>
                 <li className={styles.navitem}>
                    <Link className={styles.navLink} to="/admission">{language === 'en' ? 'ADMISSION' : '入学'}</Link>
-                </li>
+                   <ul className={styles.dropdownMenu}>
+                    <li>Apply Now</li>
+                    <li>Tuition & Fee</li>
+                    <li>FAQ</li>
+                   </ul>
+                 </li>
                 <li className={styles.navitem}>
-                  <Link className={styles.navLink} to="/support">{language === 'en' ? 'STUDENT SUPPORT' : '学生支持'}</Link>
-                </li>
+                   <Link className={styles.navLink} to="/support">{language === 'en' ? 'STUDENT SUPPORT' : '学生支持'}</Link>
+                   <ul className={styles.dropdownMenu}>
+                    <li>Academic Advising</li>
+                    <li>Life Counseling</li>
+                   </ul>
+                 </li>
                </ul>
               </div>
            </div>
