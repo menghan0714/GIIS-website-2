f25024ad59bc1412d3f5e4858ec97692a2cf2039
menghan0714 <144967883+menghan0714@users.noreply.github.com>
2024-10-19 00:28:49 +0800

    Update Nav.js

--- src/components/pages/Discovery/DiscoveryHeader/Nav.js
+++ src/components/pages/Discovery/DiscoveryHeader/Nav.js
@@ -44,9 +44,8 @@ https://drive.google.com/drive/u/0/home
                 
               <div className={`collapse navbar-collapse ${isCollapsed ? 'show' : ''}`} id="navbarSupportedContents">
                <ul className={`navbar-nav ${styles.customnavbar}`}>
-                <li className={styles.navitem2}>
-                 <p classname={styles.navText}>{language === 'en' ? 'DISCOVERY' : '发现我们'}</p>
-                </li>
+                <p className={`${styles.navitem2} ${styles.navText}`}>{language === 'en' ? 'DISCOVERY' : '发现我们'}</p>
+    
                 <li className={styles.navitem}>
                   <Link className={styles.navLink} to="/academics">{language === 'en' ? 'ACADEMICS' : '学术'}</Link>
                   <ul className={styles.dropdownMenu}>
