f33a5ea79eb7763f992be4498c5b84a3d85bdae7
menghan0714 <144967883+menghan0714@users.noreply.github.com>
2024-10-18 14:05:46 +0800

    Update Nav.js

--- src/components/pages/Discovery/DiscoveryHeader/Nav.js
+++ src/components/pages/Discovery/DiscoveryHeader/Nav.js
@@ -35,7 +35,7 @@ https://drive.google.com/drive/u/0/home
 
              <div className={`collapse navbar-collapse ${!isCollapsed ? 'show' : ''} ${styles.leftSlideMenu}`} id="navbarLeftMenu">
               <ul className={styles.leftSlideItems}>
-               <li onClick={() => window.location.href = "/discovery"}>{language === 'en' ? 'DISCOVERY' : '发现我们'}</li>
+               <p>{language === 'en' ? 'DISCOVERY' : '发现我们'}</p>
                <li onClick={() => window.location.href = "/academics"}>{language === 'en' ? 'ACADEMICS' : '学术'}</li>
                <li onClick={() => window.location.href = "/admission"}>{language === 'en' ? 'ADMISSION' : '入学'}</li>
                <li onClick={() => window.location.href = "/support"}>{language === 'en' ? 'STUDENT SUPPORT' : '学生支持'}</li>
