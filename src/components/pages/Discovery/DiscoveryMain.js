e48068a0a3cf2499c0348ea6debd1c498e90ea6a
menghan0714 <144967883+menghan0714@users.noreply.github.com>
2024-10-20 00:56:36 +0800

    Update DiscoveryMain.js

--- src/components/pages/Discovery/DiscoveryMain.js
+++ src/components/pages/Discovery/DiscoveryMain.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import Nav from './DiscoveryHeader/Nav.js';
+import img from '../../../img/Homepage/homepage3.png';
 
 function DiscoveryMain({ language }) {
    return (
     <>
         <div className="row">
             <Nav language={language} />
+        </div>
+      
+        <div className="card mb-1" id="discovery">
+          <div className="container">
+            <img />
+          </div>
         </div>
     </>
   );
