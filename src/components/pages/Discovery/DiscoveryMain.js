ff80cca4dafbd0e912b14899c449c413673b64df
menghan0714 <144967883+menghan0714@users.noreply.github.com>
2024-10-20 01:05:09 +0800

    Update DiscoveryMain.js

--- src/components/pages/Discovery/DiscoveryMain.js
+++ src/components/pages/Discovery/DiscoveryMain.js
@@ -3,6 +3,15 @@ import Nav from './DiscoveryHeader/Nav.js';
 import img from '../../../img/Homepage/homepage3.png';
 
 function DiscoveryMain({ language }) {
+
+   const imageStyle = {
+      width: '80%',
+      height: 'auto',
+      margin: '10px auto',
+      display: 'block';
+      justify-content: center;
+    };
+   
    return (
     <>
         <div className="row">
@@ -10,9 +19,7 @@ function DiscoveryMain({ language }) {
         </div>
       
         <div className="card mb-1" id="discovery">
-          <div className="container">
-            <img />
-          </div>
+          <img src={img} alt="Discovery" style={imageStyle} />
         </div>
     </>
   );
