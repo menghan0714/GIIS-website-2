891f8206b0abb46748aae0dd1a23fb25f4feaaeb
menghan0714 <144967883+menghan0714@users.noreply.github.com>
2024-10-19 12:26:09 +0800

    Update Nav.js

--- src/components/pages/Homepage/HomepageHeader/Nav.js
+++ src/components/pages/Homepage/HomepageHeader/Nav.js
@@ -29,7 +29,7 @@ function Nav({ language }) {
     return (
         <nav className={`navbar navbar-expand-lg ${isNavSticky ? 'fixed-top' : ''} ${styles.customBackground}`}>
             <div className={`container-fluid ${styles.navContainer}`}>
-             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContents" aria-controls="navbarSupportedContents" aria-expanded={!isCollapsed} aria-label="Toggle navigation" onClick={toggleNavbar}>
+             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" aria-expanded={!isCollapsed} aria-label="Toggle navigation" onClick={toggleNavbar}>
               <span className="navbar-toggler-icon"></span>
              </button>
 
@@ -42,7 +42,7 @@ function Nav({ language }) {
                </ul>
               </div>
                 
-             <div className={isCollapsed ? '' : ''} id="navbarSupportedContents">
+             <div className={`collapse navbar-collapse ${isCollapsed ? '' : ''}} id="navbarSupportedContents">
                <ul className={`navbar-nav ${styles.customnavbar}`}>
                 <li className={styles.navitem}>
                   <Link className={styles.navLink} to="/discovery">{language === 'en' ? 'DISCOVERY' : '发现我们'}</Link>
