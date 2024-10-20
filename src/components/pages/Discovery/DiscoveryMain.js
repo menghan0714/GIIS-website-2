444cddd61455d17c5137e17d5dc898877490db36
menghan0714 <144967883+menghan0714@users.noreply.github.com>
2024-10-20 21:45:40 +0800

    Update DiscoveryMain.js

--- src/components/pages/Discovery/DiscoveryMain.js
+++ src/components/pages/Discovery/DiscoveryMain.js
@@ -5,6 +5,7 @@ import img from '../../../img/Homepage/homepage3.png';
 function DiscoveryMain({ language }) {
 
     const containerStyle = {
+        position: 'relative',
         display: 'flex',
         justifyContent: 'center', // 水平居中
         marginTop: '0' // 與導航欄保持距離
@@ -15,6 +16,16 @@ function DiscoveryMain({ language }) {
         height: '400px', // 固定高度
         objectFit: 'cover'
     };
+
+        const textOverlayStyle = {
+        position: 'absolute',
+        bottom: '0', // 固定在圖片底部
+        backgroundColor: 'rgba(0, 0, 0, 0.7)', // 半透明黑色背景
+        color: 'white',
+        width: '35%',
+        padding: '10px',
+        textAlign: 'center'
+    };
    
    return (
     <>
@@ -24,6 +35,10 @@ function DiscoveryMain({ language }) {
       
         <div style={containerStyle}>
           <img src={img} alt="Discovery" style={imageStyle} />
+          <div style={textOverlayStyle}>
+            <h3>Discovery</h3>
+            <p>Genesis of Ideas International School is dedicated to providing students a dynamic learning environment and empowering them with the critical thinking and problem-solving skills.</p>
+          </div>
         </div>
     </>
   );
