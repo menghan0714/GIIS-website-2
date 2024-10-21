3ca5f56dd867cd280b3c56d8da7db2a2bb1b0792
menghan0714 <144967883+menghan0714@users.noreply.github.com>
2024-10-21 16:58:57 +0800

    Update discoveryIntroduction.js

--- src/components/pages/Discovery/Discovery/discoveryIntroduction.js
+++ src/components/pages/Discovery/Discovery/discoveryIntroduction.js
@@ -5,13 +5,13 @@ import styles from './discoveryIntroduction.css';
 function discoveryIntroduction({ language }) {
 
     const discoveryintriductionStyle = {
-        marginTop: '80px',
+        marginTop: '105px',
         color: 'white',
         width: '100%',
         display: 'flex',         // 使用flexbox
         flexDirection: 'column', // 垂直方向排列
         alignItems: 'flex-start', // 元素內部文字靠左對齊
-        paddingLeft: '200px', 
+        paddingLeft: '300px', 
         fontFamily: 'Inter, sans-serif',
         fontWeight: 'bold',
         fontSize: '50px',
