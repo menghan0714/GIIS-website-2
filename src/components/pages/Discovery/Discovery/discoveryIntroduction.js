c8a7c3180b5798326648e22a1e2b4cbb8b7b5d90
menghan0714 <144967883+menghan0714@users.noreply.github.com>
2024-10-21 15:47:15 +0800

    Update discoveryIntroduction.js

--- src/components/pages/Discovery/Discovery/discoveryIntroduction.js
+++ src/components/pages/Discovery/Discovery/discoveryIntroduction.js
@@ -5,8 +5,8 @@ import styles from './discoveryIntroduction.js';
 function discoveryIntroduction({ language }) {
     return (
      <>
-        <div className={styles.headline}>
-           GENESIS OF IDEAS INTERNATIONAL SCHOOL
+        <div className="container">
+          <p classname={styles.headline}>GENESIS OF IDEAS INTERNATIONAL SCHOOL</p>
         </div>
         
         <div>
