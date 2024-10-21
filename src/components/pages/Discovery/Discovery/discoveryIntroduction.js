6e0967882abc37dfda852c89f55e5665bae76738
menghan0714 <144967883+menghan0714@users.noreply.github.com>
2024-10-22 01:31:21 +0800

    Update discoveryIntroduction.js

--- src/components/pages/Discovery/Discovery/discoveryIntroduction.js
+++ src/components/pages/Discovery/Discovery/discoveryIntroduction.js
@@ -8,7 +8,7 @@ function discoveryIntroduction({ language }) {
         marginTop: '115px',
         color: 'white',
         width: '100%',
-        paddingLeft: '40%',
+        paddingLeft: '20%',
         fontFamily: 'Inter, sans-serif',
         fontWeight: 'bold',
         fontSize: '60px',
