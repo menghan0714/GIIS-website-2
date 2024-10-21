2376f8953fe610782330345ff6194aa8800bb55f
menghan0714 <144967883+menghan0714@users.noreply.github.com>
2024-10-21 16:38:01 +0800

    Update discoveryIntroduction.js

--- src/components/pages/Discovery/Discovery/discoveryIntroduction.js
+++ src/components/pages/Discovery/Discovery/discoveryIntroduction.js
@@ -12,7 +12,7 @@ function discoveryIntroduction({ language }) {
         zIndex: '20', 
         fontFamily: 'Inter, sans-serif',
         fontWeight: 'bold',
-        fontSize: '80px',
+        fontSize: '10px',
     };
  
     return (
