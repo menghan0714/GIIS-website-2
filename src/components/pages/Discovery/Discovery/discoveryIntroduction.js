7b797660927073edae70f706021f4675cb5224b0
menghan0714 <144967883+menghan0714@users.noreply.github.com>
2024-10-22 20:33:59 +0800

    Update discoveryIntroduction.js

--- src/components/pages/Discovery/Discovery/discoveryIntroduction.js
+++ src/components/pages/Discovery/Discovery/discoveryIntroduction.js
@@ -73,10 +73,10 @@ function discoveryIntroduction({ language }) {
                         GIIS is a fully online, globalized modern institution dedicated to providing world-class education, attracting elite students from around the world. Our educational model breaks the geographical constraints of traditional education, allowing students to access top-notch educational resources anytime, anywhere.
                     </p>
                 )}
+         <div style={lineStyle}></div>
          </div>
         </div>
 
-        <div style={lineStyle}></div>
       </>
    );
 }
