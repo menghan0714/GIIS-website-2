d72e52d032f9527d50246977a84cf605c666260e
menghan0714 <144967883+menghan0714@users.noreply.github.com>
2024-10-22 09:57:31 +0800

    Update discoveryIntroduction.js

--- src/components/pages/Discovery/Discovery/discoveryIntroduction.js
+++ src/components/pages/Discovery/Discovery/discoveryIntroduction.js
@@ -28,7 +28,7 @@ function discoveryIntroduction({ language }) {
 
     const text1 = {
         marginTop: '100px',
-        paddingRight: '40%',
+        justifyContent: 'center',
         width: '800px',  /* 設置固定寬度 */
         padding: '10px',
         wordWrap: 'break-word', /* 自動換行過長的字詞 */
