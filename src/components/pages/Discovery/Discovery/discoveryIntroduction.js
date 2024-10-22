bb19822712706cdb1b4a2dd526fc92615ef1a48d
menghan0714 <144967883+menghan0714@users.noreply.github.com>
2024-10-22 21:38:08 +0800

    Update discoveryIntroduction.js

--- src/components/pages/Discovery/Discovery/discoveryIntroduction.js
+++ src/components/pages/Discovery/Discovery/discoveryIntroduction.js
@@ -75,6 +75,7 @@ function discoveryIntroduction({ language }) {
         display: 'flex',
         justifyContent: 'center', // 水平居中
         width: '100%',
+        marginBottom: '20px',
     };
 
     const imageStyle = {
