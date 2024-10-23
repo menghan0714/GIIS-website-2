0fb812b9db95215ac89a9b14011b4bb61861407d
menghan0714 <144967883+menghan0714@users.noreply.github.com>
2024-10-23 14:10:43 +0800

    Update discoveryintroduction2.js

--- src/components/pages/Discovery/Discovery/discoveryintroduction2.js
+++ src/components/pages/Discovery/Discovery/discoveryintroduction2.js
@@ -1,1 +1,56 @@
+import React from 'react';
+import img from '../../../../img/cognia.png';
 
+function discoveryIntroduction2({ language }) {
+
+    const headlineStyle = {
+        marginTop: '53px',
+        color: 'rgba(0, 0, 0, 1)',
+        width: '60%',
+        paddingLeft: '10%',
+        fontFamily: 'Inter, sans-serif',
+        fontWeight: 'bold',
+        fontSize: '40px',
+    };
+
+      const headline2 = {
+        marginTop: '135px',
+        color: 'rgba(0, 0, 0, 1)',
+        width: '25%',
+        paddingLeft: '10%',
+        fontFamily: 'Inter, sans-serif',
+        fontWeight: 'bold',
+        fontSize: '35px',
+    };
+
+      const headlineStyle2 = {
+        marginTop: '53px',
+        color: 'rgba(0, 0, 0, 1)',
+        width: '60%',
+        paddingLeft: '10%',
+        fontFamily: 'Inter, sans-serif',
+        fontWeight: 'bold',
+        fontSize: '40px',
+    };
+
+   
+    return (
+      <>
+        <div style={headlineStyle}>
+          <p>Core values</p>
+        </div>
+
+        <div style={headline2}>
+          <p>PERSONALIZED LEARNING</p>  
+          <p>COMPREHENSIVE DEVELOPMENT</p>
+          <p>INTERNATIONAL PERSPECTIVE</p>
+        </div>
+
+        <div style={Mission}>
+          <p>Core values</p>
+        </div>
+      </>
+   );
+}
+
+export default discoveryIntroduction2;
