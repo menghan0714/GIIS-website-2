64356187e694b4d1b9e4b1fa90eb266eb5edfe50
menghan0714 <144967883+menghan0714@users.noreply.github.com>
2024-10-24 00:50:49 +0800

    Update Nav.js

--- src/components/pages/Homepage/HomepageHeader/Nav.js
+++ src/components/pages/Homepage/HomepageHeader/Nav.js
@@ -6,6 +6,7 @@ function Nav({ language }) {
     const [isNavSticky, setIsNavSticky] = useState(false);
     const [isCollapsed, setIsCollapsed] = useState(true); 
     const navigate = useNavigate();
+    const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
 
     useEffect(() => {
         const handleScroll = () => {
@@ -13,12 +14,16 @@ function Nav({ language }) {
             setIsNavSticky(currentScrollPosition > 150);
         };
 
+        const handleResize = () => {
+        setIsMobile(window.innerWidth <= 768);
+       };
 
         window.addEventListener('scroll', handleScroll);
+        window.addEventListener('resize', handleResize);
         
         return () => {
             window.removeEventListener('scroll', handleScroll);
-
+            window.removeEventListener('resize', handleResize);
         };
     }, []);
 
@@ -34,6 +39,7 @@ function Nav({ language }) {
                 <span className="navbar-toggler-icon"></span>
             </button>
 
+           {isMobile && (
             <div className={`collapse navbar-collapse ${!isCollapsed ? 'show' : ''} ${styles.leftSlideMenu}`} id="navbarLeftMenu">
                 <ul className={styles.leftSlideItems}>
                   <li onClick={() => navigate("/discovery")}>
