05d5f6dae92629efa25ff3f52b022ba75894a785
menghan0714 <144967883+menghan0714@users.noreply.github.com>
2024-10-23 18:54:54 +0800

    Update discoveryintroduction2.js

--- src/components/pages/Discovery/Discovery/discoveryintroduction2.js
+++ src/components/pages/Discovery/Discovery/discoveryintroduction2.js
@@ -13,18 +13,8 @@ function discoveryIntroduction2({ language }) {
         fontSize: '40px',
     };
 
-      const headline2 = {
-        marginTop: '135px',
-        color: 'rgba(0, 0, 0, 1)',
-        width: '25%',
-        paddingLeft: '10%',
-        fontFamily: 'Inter, sans-serif',
-        fontWeight: 'bold',
-        fontSize: '35px',
-    };
-
-      const headlineStyle2 = {
-        marginTop: '53px',
+    const headlineStyle2 = {
+        marginTop: 'px',
         color: 'rgba(0, 0, 0, 1)',
         width: '60%',
         paddingLeft: '10%',
@@ -33,22 +23,83 @@ function discoveryIntroduction2({ language }) {
         fontSize: '40px',
     };
 
-   
+
+    const headlineStyle3 = {
+        marginTop: 'px',
+        color: 'rgba(0, 0, 0, 1)',
+        width: '60%',
+        paddingLeft: '10%',
+        fontFamily: 'Inter, sans-serif',
+        fontWeight: 'bold',
+        fontSize: '40px',
+    };
+
+    const container = {
+        marginTop: 'px',
+        color: 'rgba(0, 0, 0, 1)',
+        width: '25%',
+        height: '20%',
+        paddingLeft: '10%',
+        fontFamily: 'Inter, sans-serif',
+        fontWeight: 'bold',
+        fontSize: '35px',
+        wordWrap: 'break-word',
+    };
+
+    const container2 = {
+        marginTop: 'px',
+        color: 'rgba(0, 0, 0, 1)',
+        width: '%',
+        height: '%',
+        paddingLeft: '10%',
+        fontFamily: 'Inter, sans-serif',
+        fontWeight: 'bold',
+        fontSize: '35px',
+        wordWrap: 'break-word',
+    };
+
+    const container3 = {
+        marginTop: 'px',
+        color: 'rgba(0, 0, 0, 1)',
+        width: '%',
+        height: '%',
+        paddingLeft: '10%',
+        fontFamily: 'Inter, sans-serif',
+        fontWeight: 'bold',
+        fontSize: '35px',
+        wordWrap: 'break-word',
+    };
+
+    
     return (
       <>
         <div style={headlineStyle}>
           <p>Core values</p>
         </div>
 
-        <div style={headline2}>
+        <div style={container}>
           <p>PERSONALIZED LEARNING</p>  
           <p>COMPREHENSIVE DEVELOPMENT</p>
           <p>INTERNATIONAL PERSPECTIVE</p>
         </div>
 
-        <div style={Mission}>
-          <p>Core values</p>
+        <div style={headlineStyle2}>
+          <p> </p>
         </div>
+
+        <div style={container2}>
+          <p> </p>
+        </div>
+
+        <div style={headlineStyle3}>
+          <p> </p>
+        </div>
+
+        <div style={container3}>
+          <p> </p>
+        </div>
+
+        
       </>
    );
 }
