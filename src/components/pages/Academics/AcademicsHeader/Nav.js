966852266da340ce05472c40db903e6d2bfe99b5
menghan0714 <144967883+menghan0714@users.noreply.github.com>
2024-10-24 15:43:21 +0800

    Update Nav.js

--- src/components/pages/Academics/AcademicsHeader/Nav.js
+++ src/components/pages/Academics/AcademicsHeader/Nav.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import styles from './Nav.module.css'; 
+import { Link, useNavigate } from 'react-router-dom';
 
 function Nav({ language }) {
     const [isNavSticky, setIsNavSticky] = useState(false);
