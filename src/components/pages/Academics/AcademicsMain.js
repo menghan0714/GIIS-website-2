d52fdfb97b5da649813728ec36cd4df83defdb66
menghan0714 <144967883+menghan0714@users.noreply.github.com>
2024-11-06 12:11:05 +0800

    Update AcademicsMain.js

--- src/components/pages/Academics/AcademicsMain.js
+++ src/components/pages/Academics/AcademicsMain.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Nav from './AcademicsHeader/Nav.js';
 import img from '../../../img/Homepage/homepage8.png';
 import AcademicsIntroduction from'./Academics/Academicsintroduction.js';
+import AcademicsIntroduction2 from'./Academics/Academicsintroduction2.js';
 
 function AcademicsMain({ language }) {
 
