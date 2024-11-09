1c750d84645068bc4e3ca70ed6ea5e90f3e551b0
menghan0714 <144967883+menghan0714@users.noreply.github.com>
2024-11-10 00:23:51 +0800

    Update Academicsintroduction2.js

--- src/components/pages/Academics/Academics/Academicsintroduction2.js
+++ src/components/pages/Academics/Academics/Academicsintroduction2.js
@@ -44,7 +44,7 @@ function Academicsintroduction2({ language }) {
         height: '300px', // Adjust size as needed
         backgroundColor: 'rgba(213, 168, 54, 1)', // Set to yellow color
         top: '200px',     
-        left: '700px', 
+        left: '600px', 
     };
 
     
