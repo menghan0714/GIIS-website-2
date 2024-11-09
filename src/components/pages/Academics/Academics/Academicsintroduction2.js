f09b1683757f42bf4e87194a3111ff922b2e924b
menghan0714 <144967883+menghan0714@users.noreply.github.com>
2024-11-10 00:33:54 +0800

    Update Academicsintroduction2.js

--- src/components/pages/Academics/Academics/Academicsintroduction2.js
+++ src/components/pages/Academics/Academics/Academicsintroduction2.js
@@ -66,6 +66,7 @@ function Academicsintroduction2({ language }) {
         </div>
 
         <div style={containerStyle}>
+          <div style={yellowSquareStyle}></div>
           <div style={courseintroductionStyle}>
            {language === 'zh' ? (
                     <p>
@@ -76,7 +77,6 @@ function Academicsintroduction2({ language }) {
                        Our school offers a diverse range of international programs, including the British IGCSE, A-level, and American AP courses, meeting students' academic needs. These programs provide a strong foundation for boosting university admissions chances to top institutions worldwide. By deepening their knowledge and developing critical thinking skills, our students are well-prepared to excel on the global academic stage.
                     </p>
                 )}  
-           <div style={yellowSquareStyle}></div>
            <div style={lineStyle}></div>
           </div>
         </div>
