fde6d342773ce063891bed7646336a5beb710ca1
menghan0714 <144967883+menghan0714@users.noreply.github.com>
2024-11-11 00:59:17 +0800

    Update Academicsintroduction2.js

--- src/components/pages/Academics/Academics/Academicsintroduction2.js
+++ src/components/pages/Academics/Academics/Academicsintroduction2.js
@@ -14,6 +14,11 @@ function Academicsintroduction2({ language }) {
         lineHeight: '1',
     };
 
+    const rowStyle = {
+       display: 'flex',
+       alignItems: 'center',
+    };
+    
     const containerStyle= {
         marginTop: '50px',
         width: '90%',
@@ -65,7 +70,8 @@ function Academicsintroduction2({ language }) {
         </div>
 
         <div style={containerStyle}>
-          <div style={courseintroductionStyle}>
+          <div style={rowStyle}>
+           <div style={courseintroductionStyle}>
            {language === 'zh' ? (
                     <p>
                        我们的学校提供多元化的国际课程，包括英国IGCSE、A-Level和美国AP课程，满足学生的学术需求。这些课程为提升学生进入世界顶尖大学的机会奠定了坚实基础。通过深化知识和培养批判性思维能力，我们的学生为在全球学术舞台上取得优异成绩做好了充分准备。
@@ -75,8 +81,9 @@ function Academicsintroduction2({ language }) {
                        Our school offers a diverse range of international programs, including the British IGCSE, A-level, and American AP courses, meeting students' academic needs. These programs provide a strong foundation for boosting university admissions chances to top institutions worldwide. By deepening their knowledge and developing critical thinking skills, our students are well-prepared to excel on the global academic stage.
                     </p>
                 )}  
+            <div style={yellowSquareStyle}></div>
+           </div>
            <div style={lineStyle}></div>
-           <div style={yellowSquareStyle}></div>
           </div>
         </div>
         <div style={headline2Style}>
