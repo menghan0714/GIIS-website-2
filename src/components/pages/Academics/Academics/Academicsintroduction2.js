15ec362e29d8a2d322e4211f174daeb0345681d3
menghan0714 <144967883+menghan0714@users.noreply.github.com>
2024-11-12 00:56:09 +0800

    Update Academicsintroduction2.js

--- src/components/pages/Academics/Academics/Academicsintroduction2.js
+++ src/components/pages/Academics/Academics/Academicsintroduction2.js
@@ -44,7 +44,7 @@ function Academicsintroduction2({ language }) {
         top: '20%', 
         left: '100%', 
         marginLeft: language === 'zh' ? '5%' : '',
-        top: language === 'zh' ? '15%' : '30%',
+        top: language === 'zh' ? '15%' : '50%',
     };
 
     const headline2Style = {
