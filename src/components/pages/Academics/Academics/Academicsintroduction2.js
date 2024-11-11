84d88ac9f23765ff626c1c449da4261bc14e74cc
menghan0714 <144967883+menghan0714@users.noreply.github.com>
2024-11-12 00:40:17 +0800

    Update Academicsintroduction2.js

--- src/components/pages/Academics/Academics/Academicsintroduction2.js
+++ src/components/pages/Academics/Academics/Academicsintroduction2.js
@@ -32,7 +32,7 @@ function Academicsintroduction2({ language }) {
         height: '4px',
         paddingLeft: '10%',
         backgroundColor: 'rgba(0, 0, 0, 1)', 
-        marginTop: '50%',
+        marginTop: '35px',
     };
 
     const yellowSquareStyle = {
