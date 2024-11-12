6849da15a7ae57fa3b804b1cb3aed07fcf65a948
menghan0714 <144967883+menghan0714@users.noreply.github.com>
2024-11-12 12:22:33 +0800

    Update Academicsintroduction2.js

--- src/components/pages/Academics/Academics/Academicsintroduction2.js
+++ src/components/pages/Academics/Academics/Academicsintroduction2.js
@@ -110,8 +110,8 @@ function Academicsintroduction2({ language }) {
          </div>
          <div style={containerStyle}>
            <img src={img} alt="Math" style={imageStyle} />
+           <div style={rectangleStyle}></div>
          </div>
-         <div style={rectangleStyle}></div>
       </>
    );
 }
