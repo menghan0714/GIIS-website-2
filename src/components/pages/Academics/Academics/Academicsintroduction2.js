99b7885ff3ce9b501826bbfa64293083a9d58b2e
menghan0714 <144967883+menghan0714@users.noreply.github.com>
2024-11-12 13:08:19 +0800

    Update Academicsintroduction2.js

--- src/components/pages/Academics/Academics/Academicsintroduction2.js
+++ src/components/pages/Academics/Academics/Academicsintroduction2.js
@@ -83,7 +83,7 @@ function Academicsintroduction2({ language }) {
         justifyContent: 'flex-end', 
         marginTop: '28%', 
         width: '100%',
-        paddingLeft: '30%',
+        paddingLeft: '20%',
         zIndex: '200',
     };
 
