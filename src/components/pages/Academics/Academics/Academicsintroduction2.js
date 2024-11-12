33ca7e9d49cbfb085c715a8f01364568dc3881df
menghan0714 <144967883+menghan0714@users.noreply.github.com>
2024-11-12 16:34:45 +0800

    Update Academicsintroduction2.js

--- src/components/pages/Academics/Academics/Academicsintroduction2.js
+++ src/components/pages/Academics/Academics/Academicsintroduction2.js
@@ -115,8 +115,8 @@ function Academicsintroduction2({ language }) {
     height: '100px',
     border: '3px solid black', 
     borderRadius: '50%',      
-    paddingLeft: '80%',
-    paddingTop: '50%',
+    marginLeft: '80%',
+    marginTop: '50%',
 };
 
 const arrowStyle = {
