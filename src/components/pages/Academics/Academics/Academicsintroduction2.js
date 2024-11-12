9a4ec3c866003c610c54087f9808fa607606fa6e
menghan0714 <144967883+menghan0714@users.noreply.github.com>
2024-11-12 16:44:24 +0800

    Update Academicsintroduction2.js

--- src/components/pages/Academics/Academics/Academicsintroduction2.js
+++ src/components/pages/Academics/Academics/Academicsintroduction2.js
@@ -73,7 +73,7 @@ function Academicsintroduction2({ language }) {
         width: '100%', // 保持容器的寬度
         height: '400px', // 固定高度
         objectFit: 'cover',
-        filter: 'blur(6px)',
+        opacity: '0.5', 
         zIndex: '100',
     };
 
