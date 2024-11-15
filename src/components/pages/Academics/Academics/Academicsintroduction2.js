0b354b469bb1b678e2653c19baade23ea0dd8954
menghan0714 <144967883+menghan0714@users.noreply.github.com>
2024-11-15 12:00:16 +0800

    Update Academicsintroduction2.js

--- src/components/pages/Academics/Academics/Academicsintroduction2.js
+++ src/components/pages/Academics/Academics/Academicsintroduction2.js
@@ -59,12 +59,12 @@ function Academicsintroduction2({ language }) {
         display: 'flex',
         overflow: 'hidden', // 隱藏滾動條
         clipPath: 'inset(0)',
-        width: '100vh',
+        width: '30vh',
         cursor: isDragging ? 'grabbing' : 'grab',
     };
 
    const courseBoxStyle = {
-        minWidth: '30vh',
+        minWidth: '100vh',
         height: '300px',
         backgroundColor: 'black',
         color: 'white',
