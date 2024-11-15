33901cfeafd64e62935e054b8d272ab7fdf8aa88
menghan0714 <144967883+menghan0714@users.noreply.github.com>
2024-11-15 11:41:33 +0800

    Update Academicsintroduction2.js

--- src/components/pages/Academics/Academics/Academicsintroduction2.js
+++ src/components/pages/Academics/Academics/Academicsintroduction2.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import img from '../../../../img/Academics/Math.jpg';
 
 function Academicsintroduction2({ language }) {
