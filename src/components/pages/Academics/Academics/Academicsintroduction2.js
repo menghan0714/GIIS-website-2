f6f15ae8f4acb33bc7fc12e0cb334f0c4ab42e69
menghan0714 <144967883+menghan0714@users.noreply.github.com>
2024-11-15 21:02:04 +0800

    Update Academicsintroduction2.js

--- src/components/pages/Academics/Academics/Academicsintroduction2.js
+++ src/components/pages/Academics/Academics/Academicsintroduction2.js
@@ -6,11 +6,13 @@ function Academicsintroduction2({ language }) {
     const courses1 = [
         { title: 'IGCSE', content: 'Challenge yourself with the globally recognized Pearson Edexcel International GCSEs.' , content2: 
         'These courses provide a comprehensive curriculum, designed to develop students analytical skills, creativity, and problem-solving abilities across various subjects, preparing them for future academic success.'},
-
+    ];
+    
     const courses2 = [  
         { title: 'A-Level', content: 'Our A-Level courses build on the foundation of IGCSE, providing a rigorous academic experience that sharpens critical thinking, enhances subject mastery, and prepares students for the demands of university-level education.', content2:
         'With a broad range of subjects to choose from, students can tailor their studies to suit their academic and career aspirations.'},
-
+    ];
+    
     const courses3 = [   
         { title: 'AP', content: 'Empower yourself with the globally recognized Advanced Placement  Program.' , content2: ' Our AP courses are designed to provide students with the opportunity to pursue college-level studies while still in high school. These courses not only prepare students for the AP exams but also foster a deeper understanding of subjects, equipping them with the skills necessary for success in university and beyond.'},
     ];
