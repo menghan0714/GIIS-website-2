a5cc327f672cb225906bec1b8b1ff7d372a9fd83
menghan0714 <144967883+menghan0714@users.noreply.github.com>
2024-11-15 12:08:28 +0800

    Update Academicsintroduction2.js

--- src/components/pages/Academics/Academics/Academicsintroduction2.js
+++ src/components/pages/Academics/Academics/Academicsintroduction2.js
@@ -58,7 +58,7 @@ function Academicsintroduction2({ language }) {
     const frameStyle = {
         position: 'absolute',
         left: '150%',
-        bottom: '0',
+        bottom: '-20px',
         display: 'flex',
         overflow: 'hidden', // 隱藏滾動條
         clipPath: 'inset(0)',
