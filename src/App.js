507b5d490de5c21392f16790bd40affbee6d22c2
menghan0714 <144967883+menghan0714@users.noreply.github.com>
2024-11-27 16:22:33 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 
