ea79a328aa5f5d871e4b125b3885b40d386e9e98
menghan0714 <144967883+menghan0714@users.noreply.github.com>
2025-01-04 13:01:25 +0800

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,12 +10,12 @@ function Header({ language, toggleLanguage }) {
         <header className={`${styles.header}`}>
             {/* First row for logo and buttons */}
             <div className="row align-items-center">
-                <div className="custom-col d-flex align-items-center">
+                <div className="col-9 col-md-10 col-lg-10 d-flex align-items-center">
                     <Link to="/">
                         <img src={logo_slogan} alt='Excel High School Logo' className={`img-fluid ${styles.logoImage}`} />
                     </Link>
                 </div>
-                <div className="custom-col d-flex  justify-content-center">
+                <div className= "col-3 col-md-2 col-lg-2 d-flex justify-content-center" >
                     {/* <Link to="/contact" className="btn btn-link px-2">Contact Us</Link> */}
                     <a href="https://moodles.genesisideas.school" target="_blank" rel="noopener noreferrer" className={`btn btn-link px-2 ${styles.button}`}>Moodles</a>
                     <button className={`btn btn-link px-2 ${styles.button}`} onClick={toggleLanguage}>
