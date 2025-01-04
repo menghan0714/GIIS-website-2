61bdd8e7b0269ecb6d4de1be55189635116850b9
menghan0714 <144967883+menghan0714@users.noreply.github.com>
2025-01-04 11:35:04 +0800

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,15 +10,15 @@ function Header({ language, toggleLanguage }) {
         <header className={`${styles.header}`}>
             {/* First row for logo and buttons */}
             <div className="row align-items-center">
-                <div className="col-8 col-md-10 col-lg-10 d-flex align-items-center">
+                <div className="col-9 col-md-10 col-lg-10 d-flex align-items-center">
                     <Link to="/">
                         <img src={logo_slogan} alt='Excel High School Logo' className={`img-fluid ${styles.logoImage}`} />
                     </Link>
                 </div>
-                <div className="col-4 col-md-2 col-lg-2 d-flex  justify-content-center">
+                <div className="col-3 col-md-2 col-lg-2 d-flex  justify-content-center">
                     {/* <Link to="/contact" className="btn btn-link px-2">Contact Us</Link> */}
-                    <a href="https://moodles.genesisideas.school" target="_blank" rel="noopener noreferrer" className="btn btn-link px-2">Moodles</a>
-                    <button className="btn btn-link px-2" onClick={toggleLanguage}>
+                    <a href="https://moodles.genesisideas.school" target="_blank" rel="noopener noreferrer" className=`btn btn-link px-2 ${styles.button}`>Moodles</a>
+                    <button className=`btn btn-link px-2 ${styles.button}` onClick={toggleLanguage}>
                         {language === 'en' ? 'Switch to Chinese' : '切换到英文'}
                     </button>
                 </div>
