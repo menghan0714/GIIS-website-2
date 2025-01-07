ad619fe96131ea4c8edc2c615bd1e370e9d54a9a
menghan0714 <144967883+menghan0714@users.noreply.github.com>
2025-01-07 14:18:50 +0800

    Update Slogan.js

--- src/components/pages/Homepage/Homepage/Slogan.js
+++ src/components/pages/Homepage/Homepage/Slogan.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import styles from './Slogan.module.css';
 
 function Slogan() {
     return (
-        <div className={`text-white text-center p-4 my-4 rounded ${styles.customCard}`}>
+        <div className="text-white text-center p-4 my-4 rounded style={{backgroundColor: rgba(213, 168, 54, 1)}}">
             <p className="lead" style={{ fontFamily: '"Lato", sans-serif' }}>Empowering the next generation of innovators and thinkers</p>
             {/* Replace button text and class based on the action you choose */}
             <button className="btn btn-light mt-3">Learn More</button>
