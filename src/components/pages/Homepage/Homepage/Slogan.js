66cb1baf56f23d8ff84a91aace4c0f606dbff54b
menghan0714 <144967883+menghan0714@users.noreply.github.com>
2025-01-07 16:08:27 +0800

    Update Slogan.js

--- src/components/pages/Homepage/Homepage/Slogan.js
+++ src/components/pages/Homepage/Homepage/Slogan.js
@@ -23,11 +23,11 @@ function Slogan() {
               backgroundColor: 'rgba(213, 168, 54, 1)',
               margin: '0 auto', // 居中
               display: 'flex', // 使用 flex 進行布局
-              justifyContent: 'center', // 水平居中
+              justifyContent: 'flex-start', // 水平居中
               alignItems: 'center', // 垂直居中
               textAlign: 'flex-start', // 文字居中對齊
               fontSize: '1.5em', // 調整字體大小
-              lineHeight: '1.2', // 調整行高
+              lineHeight: '1.5', // 調整行高
           }
         : {
               width: '100%', // 大於736px時的寬度
