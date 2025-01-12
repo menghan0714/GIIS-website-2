d6a93831266d0baabacbfdafbf5b1270404e270a
menghan0714 <144967883+menghan0714@users.noreply.github.com>
2025-01-12 11:32:06 +0800

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,24 @@
+import React, { useState,uesEffect} from 'react';
 import styles from './Footer.module.css';
 
 function Footer() {
+    const [isLandscape, setIsLandscape] = useState(window.matchMedia('(orientation: landscape)').matches);
+
+    useEffect(() => {
+        
+     const handleOrientationChange = () => {
+         setIsLandscape(window.matchMedia('(orientation: landscape)').matches);
+        };
+
+     window.addEventListener('resize', handleOrientationChange);
+
+     return () => {
+        window.removeEventListener('resize', handleOrientationChange);
+      };
+   }, []);
+    
     return (
-        <footer className={styles.pageFooter}>
+        <footer className={`${isLandscape ? styles.leftSlideMenu2 : styles.leftSlideMenu} ${isLandscape ? styles.leftSlideMenu2 : styles.leftSlideMenu}`>
             Copyright&copy; {new Date().getFullYear()} Genesis of Ideas International School. All rights reserved.
         </footer>
     )
