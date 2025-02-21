7e07ff82ac18868a16c485d2553a84c4003ea1e3
menghan0714 <144967883+menghan0714@users.noreply.github.com>
2025-02-21 16:00:49 +0800

    Update DiscoveryIntroduction.js

--- src/components/pages/Discovery/Discovery/DiscoveryIntroduction.js
+++ src/components/pages/Discovery/Discovery/DiscoveryIntroduction.js
@@ -2,11 +2,11 @@ import React, { useState, useEffect } from 'react';
 import img from '../../../../img/cognia.png';
 
 function DiscoveryIntroduction({ language }) {
-    const [fontSize, setFontSize] = useState(window.innerWidth < 1100 ? "60px" : "70px");
+    const [fontSize, setFontSize] = useState(window.innerWidth < 1100 ? "50px" : "70px");
 
     useEffect(() => {
     const handleResize = () => {
-      setFontSize(window.innerWidth < 1100 ? "60px" : "70px");
+      setFontSize(window.innerWidth < 1100 ? "50px" : "70px");
     };
 
     window.addEventListener("resize", handleResize);
