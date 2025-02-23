bea5069155c2a42cb2bc751b6cc9bfb22737d26b
menghan0714 <144967883+menghan0714@users.noreply.github.com>
2025-02-23 22:30:18 +0800

    Update Academicsintroduction2.js

--- src/components/pages/Academics/Academics/Academicsintroduction2.js
+++ src/components/pages/Academics/Academics/Academicsintroduction2.js
@@ -190,7 +190,7 @@ function Academicsintroduction2({ language }) {
         width: '300px',  // Adjust size as needed
         height: '300px', // Adjust size as needed
         backgroundColor: 'rgba(213, 168, 54, 1)',   
-        marginLeft: language === 'zh' ? '5%' : '5%',
+        marginLeft: language === 'zh' ? '100%' : '100%',
         top: language === 'zh' ? '20%' : '50%',
     };
 
