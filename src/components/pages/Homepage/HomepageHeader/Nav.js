156d0690bda9a99aa53638507581e77ca0430545
menghan0714 <144967883+menghan0714@users.noreply.github.com>
2025-02-23 17:52:39 +0800

    Update Nav.js

--- src/components/pages/Homepage/HomepageHeader/Nav.js
+++ src/components/pages/Homepage/HomepageHeader/Nav.js
@@ -60,24 +60,16 @@ function Nav({ language ,toggleLanguage}) {
             <div className={`collapse navbar-collapse ${!isCollapsed ? 'show' : ''} ${isLandscape ? styles.leftSlideMenu2 : styles.leftSlideMenu}`}  id="navbarLeftMenu">
              <ul className={styles.leftSlideItems} >
                   <li onClick={() => navigate("/discovery")} >
-                   <Link to="/discovery" onClick={(e) => e.preventDefault()}>
                     {language === 'en' ? 'DISCOVERY' : '发现我们'}
-                   </Link>
                   </li>
                   <li onClick={() => navigate("/academics")}>
-                    <Link to="/academics" onClick={(e) => e.preventDefault()}>
                         {language === 'en' ? 'ACADEMICS' : '学术'}
-                    </Link>
                   </li>
                   <li onClick={() => navigate("/admission")}>
-                    <Link to="/admission" onClick={(e) => e.preventDefault()}>
                         {language === 'en' ? 'ADMISSION' : '入学'}
-                    </Link>
                   </li>
                   <li onClick={() => navigate("/support")}>
-                    <Link to="/support" onClick={(e) => e.preventDefault()}>
                         {language === 'en' ? 'STUDENT SUPPORT' : '学生支持'}
-                    </Link>
                   </li>
                 </ul>
             </div>
