108d37ddb508bfd5b495495bce1c05a4bb695301
menghan0714 <144967883+menghan0714@users.noreply.github.com>
2025-02-27 22:15:07 +0800

    Update DiscoveryIntroduction2.js

--- src/components/pages/Discovery/Discovery/DiscoveryIntroduction2.js
+++ src/components/pages/Discovery/Discovery/DiscoveryIntroduction2.js
@@ -27,7 +27,7 @@ function DiscoveryIntroduction2({ language }) {
     const headlineStyle3 = {
         marginTop: '60px',
         color: 'rgba(0, 0, 0, 1)',
-        width: '70%',
+        width: '100%',
         paddingLeft: '12%',
         fontFamily: 'Inter, sans-serif',
         fontWeight: 'bold',
